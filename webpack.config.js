dc08bc248ffad4b97796adfec5c509071e8b14f5
Rrrinav <rinavhansa4@gmail.com>
2025-09-28 21:45:57 +0530

    [ INIT ]

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+        publicPath: './',
+    },
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html', // your HTML template
+        }),
+    ],
+    devServer: {
+        static: path.join(__dirname, 'public'),
+        compress: true,
+        port: 8080,
+        open: true,
+    },
+};
+
